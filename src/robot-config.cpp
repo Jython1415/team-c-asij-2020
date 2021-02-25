f0b8d06084003d8c0c8e524c5442ffd443e3b3f1
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2021-02-25 16:43:14 +0900

    removed old code
    the old code was for a practice bot. we don't have a practice bot anymore

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,24 +7,11 @@ brain Brain;
 
 controller ctlr1;
 
-// #define _PRACTICE_BOT_
-#ifndef _PRACTICE_BOT_
-
 // main bot
 motor chassisLF  = motor(PORT1, true);
 motor chassisLB  = motor(PORT9, false);
 motor chassisRF = motor(PORT5, false);
 motor chassisRB = motor(PORT8, true);
-
-#else
-
-// practice bot
-motor chassisLF  = motor(PORT1, false);
-motor chassisLB  = motor(PORT13, false);
-motor chassisRF = motor(PORT9, true);
-motor chassisRB = motor(PORT18, true);
-
-#endif
 
 motor intakeL = motor(PORT20, false);
 motor intakeR = motor(PORT7, true);
